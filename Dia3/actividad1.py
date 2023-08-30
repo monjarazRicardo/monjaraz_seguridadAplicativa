c9048abbccb3e56d4b1794a814bd217c7650adb2
monjarazRicardo <143432736+monjarazRicardo@users.noreply.github.com>
2023-08-30 10:54:17 -0600

    Create actividad1.py

---
+++ Dia3/actividad1.py
@@ -0,0 +1,10 @@
+#verifica si una cadena es palindromo o no es palindromo
+cadena = "ana"
+
+
+if (cadena[::-1] == cadena [0::]):
+
+    print("Es palindromo")
+
+else:
+    print("No es palindromo")
