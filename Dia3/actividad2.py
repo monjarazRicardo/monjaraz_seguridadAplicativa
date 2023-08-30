15914e28434fca266a653636bf8f24d812d5f3c8
monjarazRicardo <143432736+monjarazRicardo@users.noreply.github.com>
2023-08-30 12:13:58 -0600

    Create actividad2.py

---
+++ Dia3/actividad2.py
@@ -0,0 +1,54 @@
+def ingresarnum1(num1,num2):
+    num1 = int(input("ingresa el numero: "))
+    
+    
+    return num1
+
+def ingresarnum2(num1,num2):
+    
+    num2= int(input("Ingresa el numero 2: "))
+    
+    return num2
+
+num1 = ingresarnum1
+num2= ingresarnum2
+
+
+
+
+def suma(num1,num2):return num1 + num2 
+
+def resta(num1,num2):return num1-num2
+
+def div(num1,num2):return num1/num2
+
+def mult(num1,num2):return num1*num2
+
+print("Calculadora\n")
+
+print("""
+  Igresa la opcion deseada 
+  1:Suma
+  2:Resta
+  3:MUltiplicacion
+  4:Division    
+      
+      """)
+    
+
+
+opcion = int(input("Ingresa la opcion: "))
+
+
+if (opcion == 1): 
+
+    print(suma(num1, num2))
+    
+if (opcion == 2): 
+    print(resta(num1, num2))
+
+if (opcion == 3): 
+    print(div(num1, num2))
+    
+if (opcion == 4): 
+    print(mult(num1, num2))
